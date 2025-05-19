29765f6295fdbbba46f4147ff6fe7abeb4367e39
Rafael Gil <cybervoid@dumbhome.uk>
2025-05-18 22:45:47 -0400

    Use local Shoelace assets for improved performance
    Replaced CDN imports of Shoelace styles and scripts with local assets referenced through a new `shoelace.ts` module. This change enhances loading speed and reduces external dependencies.

---
+++ src/scripts/shoelace.ts
@@ -0,0 +1,4 @@
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+setBasePath('/node_modules/@shoelace-style/shoelace/dist/');
+import '@shoelace-style/shoelace/dist/shoelace.js'; 
